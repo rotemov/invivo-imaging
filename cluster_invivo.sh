4c2e0c24d92496ea1d356082c9ece7f8c7980044
rotemov <albi@64821>
2020-08-19 12:07:53 +0300

    for debug

--- cluster_invivo.sh
+++ cluster_invivo.sh
@@ -2,19 +2,17 @@
 #SBATCH -J II
 #SBATCH -o II.out
 #SBATCH -e II.err
-#SBATCH -N 4
-#SBATCH -c 40
+#SBATCH -N 5
+#SBATCH -c 60
 #SBATCH --mem=32G
-#SBATCH --mail-type=ALL
+#SBATCH --mail-type=END
 #SBATCH --mail-user=rotem.ovadia@mail.huji.ac.il
-#SBATCH --export=DATA="'/ems/elsc-labs/adam-y/rotem.ovadia/Programs/invivo-imaging/Data/demo_data'"
-#DATA = "/ems/elsc-labs/adam-y/rotem.ovadia/Programs/invivo-imaging/Data/demo_data"
-echo $DATA
+#SBATCH --export=DATA="/ems/elsc-labs/adam-y/rotem.ovadia/Programs/invivo-imaging/Data/demo_data"
 
 cd denoise
 # matlab2017b -batch "main; exit"
 matlab -batch "main("$DATA"); exit"
-echo "main("$DATA"); exit"
+echo "main('"$DATA"'); exit"
 # matlab2017b -nodisplay -nosplash -r "main; exit"
 echo "Denoising done"
 
@@ -23,5 +21,5 @@ eval "$(command conda 'shell.bash' 'hook' 2> /dev/null)"
 conda activate invivo
 echo "Demix invivo env activated"
 cd ../demix
-python main.py "$DATA"
+python main.py $DATA
 echo "Demixing done"
