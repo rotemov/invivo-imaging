89147fc2aaaf46aca2e8db792e9717d1857d322d
rotemov <rotemov@gmail.com>
2020-08-25 15:52:56 +0300

    moco modified to be a function

--- full_data_bash.sh
+++ full_data_bash.sh
@@ -10,8 +10,8 @@
 #SBATCH --mail-user=rotem.ovadia@mail.huji.ac.il
 #SBATCH --export=DATA="/ems/elsc-labs/adam-y/rotem.ovadia/Programs/invivo-imaging/Data/two_cells/cell1",FN="cell1.bin",MOV_IN="movReg.tif",DETR_SPACING=5000,ROW_BLOCKS=4,COL_BLOCKS=2,STIM_DIR="",TRUNC_START=1,TRUNC_LENGTH=5000
 
-OUTPUT = $DATA"/output"
-echo $OUTPUT
+OUTPUT=$DATA"/output"
+echo OUTPUT
 
 cd denoise
 matlab -batch "main_bash('"$DATA"','"$FN"'); exit"
@@ -19,7 +19,7 @@ source ~/Programs/invivo-imaging/activate_invivo.sh
 echo "Starting denoise.py"
 echo "denoise.py $DATA $MOV_IN $DATA"/output" $DETR_SPACING $ROW_BLOCKS $COL_BLOCKS $TRUNC_START $TRUNC_LENGTH $STIM_DIR"
 python denoise.py $DATA $MOV_IN $DATA"/output" $DETR_SPACING $ROW_BLOCKS $COL_BLOCKS $TRUNC_START $TRUNC_LENGTH $STIM_DIR
-matlab -nojvm -nodisplay -nosplash -r "motion_correction('"$DATA"','"$DATA"/output""'); exit"
+matlab -batch "motion_correction('"$DATA"','"$DATA"/output""'); exit"
 echo "Denoising done"
 cd ../demix
 python main.py $DATA
