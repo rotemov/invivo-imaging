55c3cfcf3086319a36d30e3c631b874599f1ce71
rotemov <albi@64821>
2020-08-26 16:06:39 +0300

    Unstable bash denoise script

--- full_data_bash.sh
+++ full_data_bash.sh
@@ -9,17 +9,15 @@
 #SBATCH --mail-type=END
 #SBATCH --mail-user=rotem.ovadia@mail.huji.ac.il
 #SBATCH --export=DATA="/ems/elsc-labs/adam-y/rotem.ovadia/Programs/invivo-imaging/Data/demo_data",FN="raw_data.tif",MOV_IN="movReg.tif",DETR_SPACING=5000,ROW_BLOCKS=4,COL_BLOCKS=2,STIM_DIR="",TRUNC_START=1,TRUNC_LENGTH=5000
-
+PIPELINE_DIR="/ems/elsc-labs/adam-y/rotem.ovadia/Programs/invivo-imaging"
 OUTPUT=$DATA"/output"
-echo $OUTPUT
 
 cd denoise
 matlab -batch "main_bash('"$DATA"','"$FN"'); exit"
-source ~/Programs/invivo-imaging/activate_invivo.sh
+source $PIPELINE_DIR/activate_invivo.sh
 echo "Starting denoise.py"
-echo "denoise.py $DATA $MOV_IN $DATA"/output" $DETR_SPACING $ROW_BLOCKS $COL_BLOCKS $TRUNC_START $TRUNC_LENGTH $STIM_DIR"
-python denoise.py $DATA $MOV_IN $DATA"/output" $DETR_SPACING $ROW_BLOCKS $COL_BLOCKS $TRUNC_START $TRUNC_LENGTH $STIM_DIR
-matlab -batch "motion_correction('"$DATA"','"$DATA"/output""'); exit"
+python denoise.py $DATA $MOV_IN $OUTPUT $DETR_SPACING $ROW_BLOCKS $COL_BLOCKS $TRUNC_START $TRUNC_LENGTH $STIM_DIR
+matlab -batch "motion_correction('"$DATA"','"$OUTPUT"'); exit"
 echo "Denoising done"
 cd ../demix
 python main.py $DATA
