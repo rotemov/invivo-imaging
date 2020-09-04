6c6030db93b0dc1af923f43f35564c378ccad0a2
rotemov <rotemov@gmail.com>
2020-09-04 09:37:53 +0300

    Debug of demix.py

--- full_data_bash.sh
+++ full_data_bash.sh
@@ -8,19 +8,42 @@
 #SBATCH --mem=128G
 #SBATCH --mail-type=END
 #SBATCH --mail-user=rotem.ovadia@mail.huji.ac.il
-#SBATCH --export=DATA="/ems/elsc-labs/adam-y/rotem.ovadia/Programs/invivo-imaging/Data/two_cells/cell1",FN="cell1.bin",CUTOFF_POINT=0.9,CORR_TH_FIX=0.3,PATCH_SIZE=175,BG_RANK=4
+
+DATA=${1:-"/ems/elsc-labs/adam-y/rotem.ovadia/Programs/invivo-imaging/Data/two_cells/cell1"}
+FN=${2:-"cell1.bin"}
+CUTOFF_POINT=${3:-"0.9"}
+CORR_TH_FIX=${4:-'0.3'}
+PATCH_SIZE=${5:-'175'}
+BG_RANK=${6:-'4'}
+TRUNC_START=${7:-'1000'}
+TRUNC_LENGTH=${8:-'5000'}
+OUTPUT=${9:-$DATA"/output"}
+MOV_IN=${10:-"movReg.tif"}
+DETR_SPACING=${11:-'5000'}
+ROW_BLOCKS=${12:-'4'}
+COL_BLOCKS=${13:-'2'}
+STIM_DIR=${14:-""}
+
+
+echo "Paramaters: "
+echo "Data directory: "$DATA
+echo "File name: "$FN
+echo "Cutoff point: "$CUTOFF_POINT
+echo "Correlation threshold fix: "$CORR_TH_FIX
+echo "Patch size: "$PATCH_SIZE
+echo "Background rank: "$BG_RANK
+echo "Truncation start: "$TRUNC_START
+echo "Truncation length: "$TRUNC_LENGTH
+echo "Output directory: "$OUTPUT
+echo "Registered movie: "$MOV_IN
+echo "Detrending spacing: "$DETR_SPACING
+echo "Row blocks: "$ROW_BLOCKS
+echo "COL_BLOCKS: "$COL_BLOCKS
+echo "STIM_DIR: "$STIM_DIR
+
+
+
 PIPELINE_DIR="/ems/elsc-labs/adam-y/rotem.ovadia/Programs/invivo-imaging"
-OUTPUT=$DATA"/output"
-MOV_IN="movReg.tif"
-DETR_SPACING=5000
-ROW_BLOCKS=4
-COL_BLOCKS=2
-STIM_DIR=""
-TRUNC_START=1000
-TRUNC_LENGTH=13000
-
-
-
 cd denoise
 matlab -batch "main_bash('"$DATA"','"$FN"'); exit"
 source $PIPELINE_DIR/activate_invivo.sh
